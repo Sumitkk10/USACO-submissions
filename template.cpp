547b4acb14c7e080b0d7ad5d8aae615be6a632e3
Sumit Kumar <messikumar66@gmail.com>
2020-12-09 10:35:14 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define ld long double
+using namespace std;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+void solve(){
+
+}
+
+int main(){
+	fast;
+	// freopen("blist.in", "r", stdin);
+	// freopen("blist.out", "w", stdout);
+	int t = 1;
+	// cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
